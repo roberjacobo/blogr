4966d8dea87f6e33581df2b6c1dcdfd7bce945e4
Rober <robjacobox@gmail.com>
2021-06-07 16:31:58 -0500

    navbar menu mobile almost finished

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,63 +1,81 @@
 import React from "react";
 import "../App.css";
+import ArrowImg from "../assets/icon-arrow-dark.svg"
 
 const Header = () => {
   return (
     <div className="navbar-container">
       <div>
-
-      <div className="navbar">
-        <div className="left-navbar">
+        <div className="navbar">
+          <div className="left-navbar">
             <h1>Blogr</h1>
-          <ul>
-            <li>
-              <div className="dropdown">
-                <span>Product</span>
-                <div className="dropdown-content">
-                  <p>Hello World!</p>
+            <div className="mobile-navbar">
+              <ul className="mobile-navbar-content">
+                <li>
+                  <div className="dropdown-mobile">
+                    <div><span>Product</span><button className="mobile-dropdown-btn"><img src={ArrowImg} alt="arrow image" /></button></div>
+                    <div className="dropdown-content-mobile">
+                      <p>Hello World!</p>
+                    </div>
+                  </div>
+                </li>
+                <li> <span>Company</span><div><button className="mobile-dropdown-btn"><img src={ArrowImg} alt="arrow image" /></button></div></li>
+                <li><span>Connect</span><div><button className="mobile-dropdown-btn"><img src={ArrowImg} alt="arrow image" /></button></div></li>
+                <hr />
+                <li><button className="login-buttons-mobile">Login</button></li>
+                <li><button className="login-buttons-mobile">Sign Up</button></li>
+              </ul>
+            </div>
+            <ul>
+              <li>
+                <div className="dropdown">
+                  <span>Product</span>
+                  <div className="dropdown-content">
+                    <p>Hello World!</p>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li>
-              <div className="dropdown">
-                <span>Company</span>
-                <div className="dropdown-content">
-                  <p>Hello World!</p>
+              </li>
+              <li>
+                <div className="dropdown">
+                  <span>Company</span>
+                  <div className="dropdown-content">
+                    <p>Hello World!</p>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li>
-              <div className="dropdown">
-                <span>Connect</span>
-                <div className="dropdown-content">
-                  <p>Contact</p>
-                  <p>Newsletter</p>
-                  <p>LinkedIn</p>
+              </li>
+              <li>
+                <div className="dropdown">
+                  <span>Connect</span>
+                  <div className="dropdown-content">
+                    <p>Contact</p>
+                    <p>Newsletter</p>
+                    <p>LinkedIn</p>
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
             </ul>
           </div>
-        <div className="right-navbar">
-          <ul>
-            <li>
-              <button className="login-buttons">Login</button>
-            </li>
-            <li>
-              <button className="login-buttons">Sign Up</button>
-            </li>
-          </ul>
-        </div>
+          <div className="right-navbar">
+            <ul className="login-buttons-container">
+              <li>
+                <button className="login-buttons">Login</button>
+              </li>
+              <li>
+                <button className="login-buttons">Sign Up</button>
+              </li>
+            </ul>
+          </div>
+          <div className="burger-container"><input type="checkbox" className="burger-btn" /></div>
         </div>
         <div className="texts-header-container">
-        <h2>A modern publishing platform</h2>
-        <h3>Grow your audience and build your online brand</h3>
-        <div>
-          <button className="start-btn">Start for free</button>
-          <button className="learn-btn">Learn More</button>
+          <h2>A modern publishing platform</h2>
+          <h3>Grow your audience and build your online brand</h3>
+          <div>
+            <button className="start-btn">Start for free</button>
+            <button className="learn-btn">Learn More</button>
+          </div>
         </div>
       </div>
-              </div>
     </div>
   );
 };
